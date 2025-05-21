80aa71d9635f88ad2b23f28934423f73b8d257f1
Yansiqu <166083851+Yansiqu@users.noreply.github.com>
2025-05-21 19:24:15 -0300

    Estruturação do banco de dados.

--- BarbeariaAgenda/Controllers/AgendamentosController.cs
+++ BarbeariaAgenda/Controllers/AgendamentosController.cs
@@ -1,12 +1,42 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BarbeariaAgenda.Models;
 
 namespace BarbeariaAgenda.Controllers
 {
-    public class HomeController : Controller
+    public class AgendamentosController : Controller
     {
+        private readonly BarbeariaContext _context;
+
+        public AgendamentosController(BarbeariaContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
+            var agendamentos = _context.Agendamentos.Include(a => a.Barbeiro).ToList();
+            return View(agendamentos);
+        }
+
+        public IActionResult Create()
+        {
+            ViewBag.Barbeiros = _context.Barbeiros.ToList();
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Agendamento agendamento)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Agendamentos.Add(agendamento);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Barbeiros = _context.Barbeiros.ToList();
+            return View(agendamento);
         }
     }
 }
--- BarbeariaAgenda/Program.cs
+++ BarbeariaAgenda/Program.cs
@@ -1,16 +1,18 @@
+using BarbeariaAgenda.Data;
 using BarbeariaAgenda.Models;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
-builder.Services.AddDbContext<AppDbContext>(options =>
+// ? Registra o contexto correto (BarbeariaContext)
+builder.Services.AddDbContext<BarbeariaContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
+// Middleware padrão
 app.UseStaticFiles();
 app.UseRouting();
 app.MapDefaultControllerRoute();
